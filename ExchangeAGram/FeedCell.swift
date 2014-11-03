59c916774e67892b5ecacc0476d2e638a399d6e8
Guled <guled@Nexus.local>
2014-11-03 14:58:04 -0600

    New Commit

--- ExchangeAGram/FeedCell.swift
+++ ExchangeAGram/FeedCell.swift
@@ -9,5 +9,10 @@
 import UIKit
 
 class FeedCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
     
 }
--- ExchangeAGram/FeedViewController.swift
+++ ExchangeAGram/FeedViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import MobileCoreServices
+import CoreData
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController,UICollectionViewDataSource, UICollectionViewDelegate,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var feedArray: [AnyObject] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let request = NSFetchRequest(entityName: "Feeditem")
+        let appDelegate:AppDelegate = (UIApplication.sharedApplication().delegate as AppDelegate)
+        let context:NSManagedObjectContext = appDelegate.managedObjectContext!
+        feedArray = context.executeFetchRequest(request, error: nil)!
+        
+        
         // Do any additional setup after loading the view.
     }
 
@@ -21,7 +33,78 @@ class FeedViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
+    @IBAction func snapBarButtonItemTapped(sender: UIBarButtonItem) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera){
+            var cameraController = UIImagePickerController() //Special type of nav controller access media
+            cameraController.delegate = self
+            cameraController.sourceType = UIImagePickerControllerSourceType.Camera
+            
+            let mediaTypes:[AnyObject] = [kUTTypeImage]
+            cameraController.mediaTypes = mediaTypes
+            cameraController.allowsEditing = false
+            self.presentViewController(cameraController, animated: true, completion: nil)
+            
+        }else if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
+            
+            var photoLibraryController = UIImagePickerController()
+            photoLibraryController.delegate = self
+            photoLibraryController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            
+            let mediaTypes:[AnyObject] = [kUTTypeImage]
+            photoLibraryController.mediaTypes = mediaTypes
+            photoLibraryController.allowsEditing = false
+            
+            self.presentViewController(photoLibraryController, animated: true, completion: nil)
+        }else{
+            var alertView = UIAlertController(title: "Alert!", message: "Your device does not support the camera or photo library", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            alertView.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+            
+            self.presentViewController(alertView, animated: true, completion: nil)
+            
+        }
+    }
+    
+    //UIImagePickerControllerDelegate
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        
+        let image = info[UIImagePickerControllerOriginalImage] as UIImage
+        let imageData = UIImageJPEGRepresentation(image, 1.0)
+        
+        let managedObjectContext = (UIApplication.sharedApplication().delegate as AppDelegate).managedObjectContext
+        let entityDescription = NSEntityDescription.entityForName("Feeditem", inManagedObjectContext: managedObjectContext!)
+        let feedItem = FeedItem(entity: entityDescription!, insertIntoManagedObjectContext: managedObjectContext!)
+        
+        feedItem.image = imageData
+        feedItem.caption = "test caption"
+        
+        (UIApplication.sharedApplication().delegate as AppDelegate).saveContext()
+        
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    
+    
+    
+    //UICollectionViewDataSource Functions
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+    //UICollectionViewDelegate Functions
     /*
     // MARK: - Navigation
 
